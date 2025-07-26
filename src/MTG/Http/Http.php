6eaec9adae088cf01318649a98e9d9dffa63190c
Valithor Obsidion <valzargaming@gmail.com>
2025-07-26 09:49:22 -0400

    Use DIscordPHP's traits and interfaces

--- src/MTG/Http/Http.php
+++ src/MTG/Http/Http.php
@@ -13,38 +13,45 @@ declare(strict_types=1);
 
 namespace MTG\Http;
 
+use Discord\Http\DriverInterface;
 use Discord\Http\Endpoint;
-use Discord\Http\Http as DiscordHttp;
+use Discord\Http\HttpInterface;
+use Discord\Http\HttpTrait;
+use Psr\Log\LoggerInterface;
+use React\EventLoop\LoopInterface;
 use React\Promise\Deferred;
 use React\Promise\PromiseInterface;
+use SplQueue;
 
 /**
  * Discord HTTP client.
  *
  * @author Valithor Obsidion <valithor@discordphp.org>
  */
-class Http extends DiscordHttp
+class Http implements HttpInterface
 {
+    use HttpTrait;
+
     /**
      * MTG Http version.
      *
      * @var string
      */
-    public const MTG_VERSION = 'v1.0.0';
+    public const VERSION = 'v1.0.0';
 
     /**
      * Current MTG HTTP API version.
      *
      * @var string
      */
-    public const MTG_HTTP_API_VERSION = 1;
+    public const HTTP_API_VERSION = 1;
 
     /**
      * MTG API base URL.
      *
      * @var string
      */
-    public const MTG_BASE_URL = 'https://api.magicthegathering.io/v'.self::MTG_HTTP_API_VERSION;
+    public const BASE_URL = 'https://api.magicthegathering.io/v'.self::HTTP_API_VERSION;
 
     /**
      * Authentication token.
@@ -52,6 +59,93 @@ class Http extends DiscordHttp
      * @var string
      */
     private $token;
+
+    /**
+     * Logger for HTTP requests.
+     *
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * HTTP driver.
+     *
+     * @var DriverInterface
+     */
+    protected $driver;
+
+    /**
+     * ReactPHP event loop.
+     *
+     * @var LoopInterface
+     */
+    protected $loop;
+
+    /**
+     * Array of request buckets.
+     *
+     * @var Bucket[]
+     */
+    protected $buckets = [];
+
+    /**
+     * The current rate-limit.
+     *
+     * @var RateLimit
+     */
+    protected $rateLimit;
+
+    /**
+     * Timer that resets the current global rate-limit.
+     *
+     * @var TimerInterface
+     */
+    protected $rateLimitReset;
+
+    /**
+     * Request queue to prevent API
+     * overload.
+     *
+     * @var SplQueue
+     */
+    protected $queue;
+
+    /**
+     * Request queue to prevent API
+     * overload.
+     *
+     * @var SplQueue
+     */
+    protected $interactionQueue;
+
+    /**
+     * Number of requests that are waiting for a response.
+     *
+     * @var int
+     */
+    protected $waiting = 0;
+
+    /**
+     * Whether react/promise v3 is used, if false, using v2.
+     */
+    protected $promiseV3 = true;
+
+    /**
+     * Http wrapper constructor.
+     *
+     * @param string               $token
+     * @param LoopInterface        $loop
+     * @param DriverInterface|null $driver
+     */
+    public function __construct(string $token, LoopInterface $loop, LoggerInterface $logger, ?DriverInterface $driver = null)
+    {
+        $this->token = $token;
+        $this->loop = $loop;
+        $this->logger = $logger;
+        $this->driver = $driver;
+        $this->queue = new SplQueue;
+        $this->interactionQueue = new SplQueue;
+    }
 
     /**
      * Builds and queues a request.
