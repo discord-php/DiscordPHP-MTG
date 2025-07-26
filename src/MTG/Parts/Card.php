d316ee432c1bd5514c91475df9cd89508d97aca1
Valithor Obsidion <valzargaming@gmail.com>
2025-07-26 17:38:16 -0400

    Card::getRulingsAttribute

--- src/MTG/Parts/Card.php
+++ src/MTG/Parts/Card.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 
 namespace MTG\Parts;
 
+use Discord\Helpers\Collection;
+use Discord\Helpers\ExCollectionInterface;
 use Discord\Parts\Part;
 
 class Card extends Part
@@ -52,5 +54,41 @@ class Card extends Part
         'contains',
         'id',
         'multiverseid',
+
+        // The fields below are also part of the response (if not null), but cannot currently be used as query parameters
+        'names',
+        'manaCost',
+        'variations',
+        'imageUrl',
+        'watermark',
+        'border',
+        'timeshifted',
+        'hand',
+        'life',
+        'reserved',
+        'releaseDate',
+        'starter',
+        'rulings',
+        'foreignNames',
+        'printings',
+        'originalText',
+        'originalType',
+        'legalities',
+        'source'
     ];
+
+    public function getRulingsAttribute(): ?ExCollectionInterface
+    {
+        if (! isset($this->attributes['rulings']) || ! is_array($this->attributes['rulings'])) {
+            return null;
+        }
+
+        $collection = Collection::for(Ruling::class);
+
+        foreach ($this->attributes['rulings'] as $idx => $ruling) {
+            $collection->set($idx, $this->factory->part(Ruling::class, (array) $ruling));
+        }
+
+        return $collection;
+    }
 }
