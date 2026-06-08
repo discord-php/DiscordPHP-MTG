0f8c6f3e0a6b4804cbbe34d79f56c8bdc3e72ca1
Valithor Obsidion <valithor@discordphp.org>
2026-06-08 14:12:22 -0400

    Reduce nested search for .env

--- bot.php
+++ bot.php
@@ -83,15 +83,10 @@ function loadEnv(string $filePath): void
     });
 }
 
-$env_path = file_exists($env_path = getcwd().'/.env') ? $env_path
-    : (file_exists($env_path = dirname(getcwd()).'/.env') ? $env_path
-    : (file_exists($env_path = realpath(getcwd().'/../.env') ?: '') ? $env_path
-    : (file_exists($env_path = realpath(getcwd().'/../../.env') ?: '') ? $env_path
-    : (file_exists($env_path = realpath(dirname(getcwd()).'/../.env') ?: '') ? $env_path
-    : (
-        file_exists($env_path = realpath(dirname(getcwd()).'/../../.env') ?: '') ? $env_path
+$env_path = file_exists($env_path = getcwd().'/.env') ? $env_path // Ran from root directory
+    : (file_exists($env_path = dirname(getcwd()).'/.env') ? $env_path // Ran from a subdirectory
     : null
-    )))));
+);
 $env_path ? loadEnv($env_path) : throw new \Exception('The .env file does not exist. Please create one in the root directory.');
 
 $streamHandler = new StreamHandler('php://stdout', Level::Debug);
